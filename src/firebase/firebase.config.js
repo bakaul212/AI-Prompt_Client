9e9eceae6c159e8f43416a791ac498865d625306
HASAN <m.h.bakaul@kuwaitarmy.gov.kw>
2026-06-21 21:47:34 +0600

    mongodb

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -13,5 +13,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth and Google Provider, then export them
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
